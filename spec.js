556044318266dc012bd54ef4f873732d63b4f656
ericpkatz <ericpkatz@gmail.com>
2022-10-15 09:33:57 -0400

    a commit

---
+++ spec.js
@@ -0,0 +1,12 @@
+const app = require('supertest')(require('./app'));
+const { expect } = require('chai');
+
+describe('Our App', ()=> {
+  describe('our home page', ()=> {
+    it('has welcome message', async()=> {
+      const response = await app.get('/');
+      expect(response.status).to.equal(200);
+      expect(response.text).to.contain('Welcome to Acme HR');
+    });
+  });
+});
